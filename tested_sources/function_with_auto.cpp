2361e1345161df28a1466350c3834b716b3b5d2d
alex-ganyukhin <56980915+alex-ganyukhin@users.noreply.github.com>
2021-06-21 08:26:12 +0300

    Update function_with_auto.cpp
    Copy-paste mistake

--- tested_sources/function_with_auto.cpp
+++ tested_sources/function_with_auto.cpp
@@ -1,13 +1,11 @@
 template<typename T>
 concept IsBig = sizeof(T) >= 4;
 
-template<typename T>
 void foo(IsBig auto const &) { }
 
-template<typename T>
 void foo(auto const &) {}
 
 void later() {
-    foo<char>('a');
-    foo<int>(1);
+    foo('a');
+    foo(1);
 }
